e3455742e04857b95a2338e21e59fc46e6b03992
Evan Wright <evnwright@gmail.com>
2019-05-25 09:48:04 -0600

    Add the greater than operator

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// I learned the not equal operation
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
