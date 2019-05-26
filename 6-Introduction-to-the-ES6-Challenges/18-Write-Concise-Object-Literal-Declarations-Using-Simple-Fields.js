1ad35cd17690c463218d40b4b64adb6e16781d33
Evan Wright <evnwright@gmail.com>
2019-05-26 15:01:52 -0600

    Use getters and setters on classes

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,9 @@
+// I learned to use object literals to provide a key and value to an object quickly and clearer
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  {(name, age, gender)};
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,16 @@
+// I learned how the class and constructor operators work together in a function
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+  constructor (name){
+    this.name = name;
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
