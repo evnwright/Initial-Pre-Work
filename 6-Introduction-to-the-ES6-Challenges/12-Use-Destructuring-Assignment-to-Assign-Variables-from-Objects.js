768d6c32d660eaa82bac3ae6762e6c88a53236ce
Evan Wright <evnwright@gmail.com>
2019-05-26 14:02:03 -0600

    Trim code by destructuring

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+// I learned the destructuring assignment to quickly obtain values from keys and assign them to objects
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const { tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,11 @@
+// I learned how to use destructuring to change values of a variable
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a, b] = [b, a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
