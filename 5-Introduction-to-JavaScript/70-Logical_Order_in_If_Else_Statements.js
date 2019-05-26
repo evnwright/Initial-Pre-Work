92ca65422bbc1b07799454c8ea02cfda52ce7bf5
Evan Wright <evnwright@gmail.com>
2019-05-26 00:48:49 -0600

    Create a chain of if/else statements

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+// I was reminded that a function executes from top to bottom so order is important in if statements to get the correct output
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
